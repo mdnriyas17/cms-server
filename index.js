6a8080fd963e0c00c20bb04b95df76f08a0bab34
mdnriyas17 <mdnriyas@gmail.com>
2023-09-12 11:22:39 +0530

    env change

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "./config/.env" });
+require("dotenv").config({ path: "./config/config.env" });
 const express = require("express");
 const morgan = require("morgan");
 
